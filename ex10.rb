b0092b868abaa50b3caa5b04734e2b9bebcef657
Alejandro Ríos <alerios7@gmail.com>
2015-08-21 00:58:38 -0300

    Ejercicios 3-17

---
+++ ex10.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line"
+backslash_cad = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cad
+puts fat_cat
---
+++ ex9.rb
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the month: #{months}"
+puts """
+There's something going on here.
+With the three double-qoutes.
+We'll be able to type as much as we like.
+Even 4 line if we want, or 5 or 6.
+"""
